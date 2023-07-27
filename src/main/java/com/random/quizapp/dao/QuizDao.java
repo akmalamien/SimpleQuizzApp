67260207e7800ba1e1070fa2b9a763f4c5d63716
akmalamien <akmal.amien@gmail.com>
2023-07-27 12:48:24 +0800

    Initial commit

---
+++ src/main/java/com/random/quizapp/dao/QuizDao.java
@@ -0,0 +1,7 @@
+package com.random.quizapp.dao;
+
+import com.random.quizapp.model.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizDao extends JpaRepository<Quiz, Integer> {
+}
